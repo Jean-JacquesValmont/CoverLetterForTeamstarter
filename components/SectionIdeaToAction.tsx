88f686720038cc67afe11f3c33820f5d46b5f87f
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2024-07-06 15:35:36 +0200

    Updated SectionIdeaToAction Css style

--- components/SectionIdeaToAction.tsx
+++ components/SectionIdeaToAction.tsx
@@ -3,19 +3,22 @@ import Image from "next/image";
 
 const SectionIdeaToAction = () => {
   return (
-    <div className="w-[85rem] bg-[url('https://cdn.prod.website-files.com/621defa0940f1fecd5d8d53b/622b6b8e2118bede8a80ff19_confeti-bg.svg')] flex justify-center items-center align-middle pb-16 px-32">
-        <div className='border-black border-[1px] rounded-lg mr-12'>
-            <Image className="relative top-6 left-4 py-4 " src="/image/hero_image2.png" alt="image_computer_smartphone" width={700} height={500} />
-        </div>
-        <div className='flex flex-col '>
-            <h2 className=" mb-4 text-5xl font-black text-neutral-800">Une démarche pour passer <span className='text-[#ff00ac]'>de l’idée à l’action</span></h2>
-            <p className='pb-4 font-light leading-5'>Une plateforme interne à l’entreprise où chaque collaborateur peut proposer, réaliser et financer des projets. Responsabilisez vos salariés et libérez les énergies grâce à un budget mensuel alloué par l’entreprise à chacun d’entre eux.</p>
-            <div className='flex gap-4'>
-                <button className='bg-[#00FFB7] font-extrabold px-4 py-2 rounded-full transition transform duration-700 shadow-md hover:-translate-y-1 hover:shadow-custom-dark'>Voir les projets réalisés</button>
-                <button className='bg-[#64008C] text-white font-extrabold px-4 py-2 rounded-full transition transform duration-700 shadow-md hover:-translate-y-1 hover:shadow-custom-dark'>Notre accompagement</button>
+    <div className="bg-[url('https://cdn.prod.website-files.com/621defa0940f1fecd5d8d53b/622b6b8e2118bede8a80ff19_confeti-bg.svg')] flex justify-center items-center">
+        <div className="w-[85rem] flex justify-center items-center align-middle pb-16 px-32">
+            <div className='border-black border-[1px] rounded-lg mr-12'>
+                <Image className="relative top-6 left-4 py-4 " src="/image/hero_image2.png" alt="image_computer_smartphone" width={700} height={500} />
+            </div>
+            <div className='flex flex-col '>
+                <h2 className=" mb-4 text-5xl font-black text-neutral-800">Une démarche pour passer <span className='text-[#ff00ac]'>de l’idée à l’action</span></h2>
+                <p className='pb-4 font-light leading-5'>Une plateforme interne à l’entreprise où chaque collaborateur peut proposer, réaliser et financer des projets. Responsabilisez vos salariés et libérez les énergies grâce à un budget mensuel alloué par l’entreprise à chacun d’entre eux.</p>
+                <div className='flex gap-4'>
+                    <button className='bg-[#00FFB7] font-extrabold px-4 py-2 rounded-full transition transform duration-700 shadow-md hover:-translate-y-1 hover:shadow-custom-dark'>Voir les projets réalisés</button>
+                    <button className='bg-[#64008C] text-white font-extrabold px-4 py-2 rounded-full transition transform duration-700 shadow-md hover:-translate-y-1 hover:shadow-custom-dark'>Notre accompagement</button>
+                </div>
             </div>
         </div>
     </div>
+    
   )
 }
 
